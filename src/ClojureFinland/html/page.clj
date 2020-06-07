80e246cf0764a4d997832cd72666e007e3eaf33a
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2020-06-07 15:42:06 +0300

    Make 'code' syntax prettier

--- src/ClojureFinland/html/page.clj
+++ src/ClojureFinland/html/page.clj
@@ -11,18 +11,18 @@
 (def about
   {:title "About"
    :description
-   {:code   "(prn (:description page/about))"
-    :output "This is Clojure Finland. We are a community that
+   {:code "(-> page/about :description :data)"
+    :data "This is Clojure Finland. We are a community that
    blalbalbalblaba..."}})
 
 (def companies
-  {:title       "Companies"
+  {:title "Companies"
    :description
-   {:code   "(prn (:description page/companies))"
-    :output "Companies that use Clojure in Finland"}
+   {:code "(-> page/companies :description :data)"
+    :data "Companies that use Clojure in Finland"}
    :items
-   {:code   "(doseq [c (:items page/companies)] (prn (:output c)))"
-    :output
+   {:code "(doseq [company (-> page/companies :items :data)] (prn company))"
+    :data
     [{:name "Metosin"
       :web  "https://www.metosin.fi"}
      {:name "Solita"
@@ -35,12 +35,12 @@
 (def people
   {:title "People"
    :description
-   {:code   "(prn (:description page/people))"
-    :output "Following people are active members of the Clojure
+   {:code "(-> page/people :description :data)"
+    :data "Following people are active members of the Clojure
     Finland community"}
    :items
-   {:code "(doseq [p (:items page/people)] (prn (:output p)))"
-    :output
+   {:code "(-> page/people :items :data)"
+    :data
     [{:name    "Ykä"
       :github  "https://github.com/ykarikos"
       :twitter "https://twitter.com/ykarikos"}
@@ -60,15 +60,15 @@
 
 (def meetup-groups
   {:title "Meetup Groups"
-   :code  "(prn (:output page/meetup-groups))"
-   :output
+   :code  "(:data page/meetup-groups)"
+   :data
    {:helsinki "https://www.meetup.com/Helsinki-Clojure-Meetup/events/"
     :tampere  "https://www.meetup.com/Tampere-Clojure-Meetup/"}})
 
 (def contact
   {:title "Contact"
-   :code  "(prn (:output page/contact))"
-   :output
+   :code  "(:data page/contact)"
+   :data
    {:github  "https://github.com/ClojureFinland"
     :twitter "https://twitter.com/clojurefinland"
     :email   "onko.meillä@joku.osoite"
@@ -109,19 +109,19 @@
                  :else     (text-output v))]])]]]
     [:div.item-last "}"]]])
 
-(defn section [{:keys [title description code items output]}]
+(defn section [{:keys [title description code items data]}]
   (cond-> [] ; use vector to conj at the end
     title       (conj [:h2 ";; " title])
     code        (conj (code-output code))
     description (conj (section description))
     items       (conj (section items))
-    output      (conj (cond
-                        (map? output)  (map-output output)
-                        (coll? output) (->> output
-                                            (map map-output)
-                                            (interpose [:div.separator]))
-                        (link? output) (link output)
-                        :else          (text-output output)))
+    data        (conj (cond
+                        (map? data)  (map-output data)
+                        (coll? data) (->> data
+                                          (map map-output)
+                                          (interpose [:div.separator]))
+                        (link? data) (link data)
+                        :else        (text-output data)))
     :always     seq)) ; convert to seq for Hiccup
 
 ;;; Main ;;;
