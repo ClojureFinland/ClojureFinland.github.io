9eaef561cefbfd5a39cf43545465525f47928c7f
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2020-06-07 09:21:27 +0300

    Improve color contrast

--- src/ClojureFinland/css/styles.clj
+++ src/ClojureFinland/css/styles.clj
@@ -4,13 +4,19 @@
 (defn gen-styles [_]
   (garden/css
    [:body
-    {:color            "green"
+    {:color            "#55AD1B"
      :font-family      "Courier"
      :background-color "#0F0D01"
      :font-size        "14px"}]
 
+   [:a
+    {:color "#1D96AD"}]
+
+   [:a:hover
+    {:color "#21a5bf"}]
+
    [:h1 :h2
-    {:color "gray"}]
+    {:color "#99A3A4"}]
 
    [:.separator
     {:height "0.5em"}]
